a5f10898ff7dff88d02c32e30ee02363bcbc1c53
yoonk.jo_UI <gisele@lfcorp.com>
2023-11-20 12:58:25 +0900

    setup tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <>하이</>;
+  return (
+    <div className="bg-red-100">
+      <h1 className="text-blue-500">하이</h1>
+    </div>
+  );
 };
 
 export default Home;
