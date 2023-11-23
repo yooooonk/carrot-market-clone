ba1247f5d37145be01a779eba0ad44bc4bc2fcb2
yoonk.jo_UI <gisele@lfcorp.com>
2023-11-23 12:39:40 +0900

    [part 1] 컴포넌트 스타일링  연습2 - relative, - margin, -top

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,11 +17,64 @@ const Home: NextPage = () => {
           <span className="font-semibold">Total</span>
           <span className="font-semibold">$970</span>
         </div>
-        <div className="bg-blue-500 m-auto w-2/3 mt-5 text-white p-2 text-center rounded-lg">
+        <div className="bg-blue-400 m-auto w-2/3 mt-5 text-white p-2 text-center rounded-lg">
           CheckOut
         </div>
       </div>
-      <div className="bg-white p-10 rounded-3xl shadow-xl"></div>
+      <div className="bg-white rounded-3xl shadow-xl overflow-hidden">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="relative -top-5 bg-white rounded-3xl p-10">
+          <div className="flex relative -top-20 justify-between items-end">
+            {/* <img
+              className="absolute left-1/3 w-20 h-20 rounded-full"
+              src="https://img.freepik.com/premium-vector/lion-cute-character-icon-hand-drawn-vector-illustration_101266-15526.jpg"
+              alt="avartar"
+            /> */}
+
+            <div className="flex-col ">
+              <span className="block  text-center text-gray-400">Orders</span>
+              <span className="block text-center">340</span>
+            </div>
+            <div className="h-24 w-24 bg-red-400 rounded-full " />
+            <div className="flex-col">
+              <span className="block text-center text-gray-400">Spend</span>
+              <span className="block text-center">$2400</span>
+            </div>
+          </div>
+          <div className="text-center relative -mt-14 -mb-10">
+            <div className="font-semibold text-lg">Tony Molloy</div>
+            <div className="text-gray-300 font-semibold">New York, USA</div>
+          </div>
+        </div>
+        {/* <div>
+          <span>Profile</span>
+        </div>
+
+        <div className="bg-white absolute top-4/5 left-0 rounded-3xl w-full p-10">
+          <div className="flex justify-between">
+            <img
+              className="absolute left-1/3 w-20 h-20 rounded-full"
+              src="https://img.freepik.com/premium-vector/lion-cute-character-icon-hand-drawn-vector-illustration_101266-15526.jpg"
+              alt="avartar"
+            />
+
+            <div className="flex-col ">
+              <span className="block  text-center text-gray-400">Orders</span>
+              <span className="block text-center">340</span>
+            </div>
+            <div className="flex-col">
+              <span className="block text-center text-gray-400">Spend</span>
+              <span className="block text-center">$2400</span>
+            </div>
+          </div>
+          <div className="text-center">
+            <div className="font-semibold text-lg">Tony Molloy</div>
+            <div className="text-gray-300 font-semibold">New York, USA</div>
+          </div>
+        </div> */}
+      </div>
       <div className="bg-white p-10 rounded-3xl shadow-xl"></div>
       <div className="bg-white p-10 rounded-3xl shadow-xl"></div>
     </div>
