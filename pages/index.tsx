42a5e9739a81bdfbcc12aef748e93a7b4f81fc71
yoonk.jo_UI <gisele@lfcorp.com>
2023-11-23 12:58:46 +0900

    [part 1] 컴포넌트 스타일링  연습3 - 상품 카드 컴포넌트

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,35 +48,48 @@ const Home: NextPage = () => {
             <div className="text-gray-300 font-semibold">New York, USA</div>
           </div>
         </div>
-        {/* <div>
-          <span>Profile</span>
-        </div>
-
-        <div className="bg-white absolute top-4/5 left-0 rounded-3xl w-full p-10">
-          <div className="flex justify-between">
-            <img
-              className="absolute left-1/3 w-20 h-20 rounded-full"
-              src="https://img.freepik.com/premium-vector/lion-cute-character-icon-hand-drawn-vector-illustration_101266-15526.jpg"
-              alt="avartar"
-            />
-
-            <div className="flex-col ">
-              <span className="block  text-center text-gray-400">Orders</span>
-              <span className="block text-center">340</span>
-            </div>
-            <div className="flex-col">
-              <span className="block text-center text-gray-400">Spend</span>
-              <span className="block text-center">$2400</span>
-            </div>
-          </div>
-          <div className="text-center">
-            <div className="font-semibold text-lg">Tony Molloy</div>
-            <div className="text-gray-300 font-semibold">New York, USA</div>
-          </div>
-        </div> */}
       </div>
-      <div className="bg-white p-10 rounded-3xl shadow-xl"></div>
-      <div className="bg-white p-10 rounded-3xl shadow-xl"></div>
+      <div className="bg-white p-6 rounded-3xl shadow-xl">
+        <div className="flex justify-between">
+          <span>←</span>
+          <span>
+            <span className="m-5">⭐️ 4.9</span>
+            <span className="p-1 shadow-md rounded-lg">💗</span>
+          </span>
+        </div>
+        <img
+          src="https://res.cloudinary.com/castlery/image/private/b_rgb:FFFFFF,c_fit,f_auto,q_auto,w_1000/v1650364659/crusader/variants/41960009/Vincent-Chair-Walnut-Angle.jpg"
+          alt="chair"
+        />
+        <div>
+          <div className="font-semibold">Swoon Lounge</div>
+          <div className="text-sm text-gray-300">Chaire</div>
+          <div className="flex justify-between">
+            <span>
+              <input type="radio" />
+              <input type="radio" />
+              <input type="radio" />
+            </span>
+            <span>
+              <span className="bg-blue-100 w-10 h-10 px-2 py-1 rounded-md">
+                {" "}
+                -{" "}
+              </span>
+              <span className="m-2"> 1 </span>
+              <span className="bg-blue-100 w-10 h-10 px-2 py-1 rounded-md">
+                {" "}
+                +{" "}
+              </span>
+            </span>
+          </div>
+          <div className="flex justify-between mt-4">
+            <span className="text-2xl">$450</span>
+            <div className="inline-block bg-blue-500 w-1/2 rounded-lg text-center p-2 text-white">
+              Add to Cart
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
